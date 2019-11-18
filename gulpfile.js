b91ea025d8cb69d904fff615a3ad6e752623a20d
Maxim <maxim_91@inbox.ru>
2019-11-18 16:33:58 +0300

    added structure and source

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('styles', function() {
 	.pipe(sourcemaps.init({loadMaps: true}))
 		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
 		// .pipe(rename({ suffix: '.min', prefix : '' }))
-		.pipe(autoprefixer(['last 15 versions']))
+		// .pipe(autoprefixer(['last 15 versions']))
 		// .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('app/css'))
